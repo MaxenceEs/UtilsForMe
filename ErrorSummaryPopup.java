f19cff1fc6c2cc3b86c191ed65a6bd87ed3ea121
Maxence <eschaliermaxou@hotmail.fr>
2024-03-16 15:55:36 +0100

    New

--- ErrorSummaryPopup.java
+++ ErrorSummaryPopup.java
@@ -1,80 +1,108 @@
-import java.util.ArrayList;
-
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.TableLayout;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.*;
 
-public class ErrorSummaryDialog extends Dialog {
+public class ErrorSummaryPopup extends PopupDialog {
+    private TableViewer tableViewer;
 
-    private ArrayList<ErrorInfo> errorList;
-
-    protected ErrorSummaryDialog(Shell parentShell, ArrayList<ErrorInfo> errorList) {
-        super(parentShell);
-        this.errorList = errorList;
+    public ErrorSummaryPopup(Shell parentShell) {
+        super(parentShell, SWT.RESIZE, true, true, false, false, false, null, null);
     }
 
     @Override
     protected Control createDialogArea(Composite parent) {
-        Composite container = (Composite) super.createDialogArea(parent);
-        GridLayout layout = new GridLayout(1, false);
-        container.setLayout(layout);
+        Composite composite = (Composite) super.createDialogArea(parent);
+        composite.setLayout(new FillLayout());
 
-        TableViewer tableViewer = new TableViewer(container, SWT.BORDER | SWT.FULL_SELECTION);
-        tableViewer.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-        tableViewer.getTable().setHeaderVisible(true);
+        Table table = new Table(composite, SWT.BORDER | SWT.FULL_SELECTION);
+        table.setHeaderVisible(true);
+        table.setLinesVisible(true);
 
-        TableLayout tableLayout = new TableLayout();
-        tableViewer.getTable().setLayout(tableLayout);
+        tableViewer = new TableViewer(table);
+        tableViewer.setContentProvider(new ArrayContentProvider());
 
-        tableLayout.addColumnData(new ColumnWeightData(30, 150, true));
-        tableLayout.addColumnData(new ColumnWeightData(50, 200, true));
-        tableLayout.addColumnData(new ColumnWeightData(20, 100, true));
-
-        tableViewer.setContentProvider(ArrayContentProvider.getInstance());
-
-        tableViewer.setLabelProvider(new ColumnLabelProvider() {
+        // Ajoutez les colonnes avec leurs étiquettes
+        TableViewerColumn column1 = new TableViewerColumn(tableViewer, SWT.NONE);
+        column1.getColumn().setText("Type de contrôle");
+        column1.getColumn().setWidth(150);
+        column1.setLabelProvider(new ColumnLabelProvider() {
             @Override
             public String getText(Object element) {
-                ErrorInfo errorInfo = (ErrorInfo) element;
-                switch (columnIndex) {
-                    case 0:
-                        return errorInfo.getControlType();
-                    case 1:
-                        return errorInfo.getErrorMessage();
-                    case 2:
-                        return errorInfo.getErrorObjects().toString();
-                    default:
-                        return "";
-                }
+                // Récupérez le type de contrôle de l'objet
+                // et renvoyez-le en tant que texte
+                return ""; // Modifier pour récupérer la valeur réelle
             }
         });
 
-        tableViewer.setInput(errorList);
+        TableViewerColumn column2 = new TableViewerColumn(tableViewer, SWT.NONE);
+        column2.getColumn().setText("Message d'erreur");
+        column2.getColumn().setWidth(300);
+        column2.setLabelProvider(new ColumnLabelProvider() {
+            @Override
+            public String getText(Object element) {
+                // Récupérez le message d'erreur de l'objet
+                // et renvoyez-le en tant que texte
+                return ""; // Modifier pour récupérer la valeur réelle
+            }
 
-        return container;
+            @Override
+            public Color getForeground(Object element) {
+                // Vous pouvez définir la couleur du texte pour les erreurs ici
+                // par exemple, si l'erreur est critique, vous pouvez la rendre rouge
+                return super.getForeground(element);
+            }
+        });
+
+        TableViewerColumn column3 = new TableViewerColumn(tableViewer, SWT.NONE);
+        column3.getColumn().setText("Correction");
+        column3.getColumn().setWidth(100);
+        column3.setLabelProvider(new ColumnLabelProvider() {
+            @Override
+            public String getText(Object element) {
+                return "Correction";
+            }
+        });
+
+        // Créez l'EditingSupport pour la colonne de correction
+        column3.setEditingSupport(new EditingSupport(tableViewer) {
+            @Override
+            protected CellEditor getCellEditor(Object element) {
+                return new TextCellEditor(tableViewer.getTable());
+            }
+
+            @Override
+            protected boolean canEdit(Object element) {
+                // Retourne true si l'élément peut être édité
+                return true;
+            }
+
+            @Override
+            protected Object getValue(Object element) {
+                // Retournez la valeur à afficher dans la cellule
+                return "Correction";
+            }
+
+            @Override
+            protected void setValue(Object element, Object value) {
+                // Implémentez la logique pour la correction automatique ici
+                // Par exemple, vous pouvez appeler la méthode autoCorrect ici
+                // en passant l'objet à corriger
+                // assurez-vous de rafraîchir la table après la correction
+            }
+        });
+
+        return composite;
     }
 
-    @Override
-    protected void createButtonsForButtonBar(Composite parent) {
-        createButton(parent, SWT.OK, "OK", true);
+    // Méthode pour ajouter une erreur à la table
+    public void addError(String controlType, String errorMessage, ArrayList<MyClassObject> errorObjects) {
+        // Créez un objet pour représenter l'erreur et ajoutez-le à la table
+        // en utilisant le viewer
     }
 
-    @Override
-    protected void buttonPressed(int buttonId) {
-        if (buttonId == SWT.OK) {
-            close();
-        }
+    // Méthode pour déclencher la correction automatique
+    private void autoCorrect(ArrayList<MyClassObject> objectsToCorrect) {
+        // Implémentez la logique pour la correction automatique ici
     }
 }
